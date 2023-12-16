cb9301573c820981e0cf91dcf630db8c9ca54612
Nobu Tamagawa <97799865+ntamagawa@users.noreply.github.com>
2023-12-16 15:22:58 -0500

    Add Controller tests
    Found a bug by running Data Entry tests.
    Also added basic integration test - just to check the existence of controller and service instances

--- src/test/java/com/example/sumpdata/SumpdataApplicationTests.java
+++ src/test/java/com/example/sumpdata/SumpdataApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.sumpdata;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.sumpdata.data.DataEntryService;
+import com.example.sumpdata.rest.DataEntryController;
+import com.example.sumpdata.rest.ListController;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SumpdataApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DataEntryController dataEntryController;
+
+    @Autowired
+    private ListController listController;
+
+    @Autowired
+    private DataEntryService dataEntryService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+        assertThat(dataEntryController).isNotNull();
+        assertThat(listController).isNotNull();
+        assertThat(dataEntryService).isNotNull();
+    }
+
 
 }
