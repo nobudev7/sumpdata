4c55cbb6d2126eee5a3a0ff12bffae6786de9821
Nobu Tamagawa <97799865+ntamagawa@users.noreply.github.com>
2023-12-23 12:42:43 -0500

    Add files entry point unit test
    The unit test verifies the validity of the json returned by the entry point.
    The data service layer is mocked.

--- src/test/java/com/example/sumpdata/DataEntryControllerTests.java
+++ src/test/java/com/example/sumpdata/DataEntryControllerTests.java
@@ -9,16 +9,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(DataEntryController.class)
@@ -81,4 +95,26 @@ public class DataEntryControllerTests {
                 .andExpect(jsonPath("[0].measuredOn", is("2023-11-12T15:18:06")))
                 .andExpect(jsonPath("[0].value", is(152)));
     }
+
+    @Test
+    void uploadDataEntryFileShouldReturnValidUpdateStaus() throws Exception {
+
+        String filename = "waterlevel-20230805.csv";
+        File initialFile = new File("src/test/data/" + filename);
+
+        doReturn(3).when(dataEntryService).processCSV(eq(1), any(), eq(filename));
+
+        mvc.perform(multipart("/devices/{device}/entries/files", 1)
+                        .file(new MockMultipartFile("files", filename, MediaType.MULTIPART_FORM_DATA_VALUE, new FileInputStream(initialFile))))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.numFiles", is(1)))
+                .andExpect(jsonPath("$.totalEntries", is(3)))
+                .andExpect(jsonPath("$.success", is(true)))
+                .andExpect(jsonPath("$.fileStatus.[0].filename", is(filename)))
+                .andExpect(jsonPath("$.fileStatus.[0].success", is(true)))
+                .andExpect(jsonPath("$.fileStatus.[0].numEntries", is(3)))
+                .andExpect(jsonPath("$.fileStatus.[0].error", is("")));
+    }
+
 }
